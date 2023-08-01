5fbb194396147fa4180b0170c40b2e40a0411981
rodriguesrai <rairodrigues_sf@hotmail.com>
2023-08-01 11:45:03 -0300

    requisito 2 parcial

--- src/componentes/Search/Search.tsx
+++ src/componentes/Search/Search.tsx
@@ -1,6 +1,69 @@
+import { useState } from 'react';
+import Carregando from '../Carregando/carregando';
+import searchAlbumsAPI from '../../services/searchAlbumsAPI';
+
 function Search() {
+  const [value, setValue] = useState(''); // useState do input
+  const [loading, setLoading] = useState(false); // useState do carregando
+  const [showInput, setShowInput] = useState(true); // esconde input
+  const [showResult, setShowResult] = useState(false);
+  const [artistName, setArtistName] = useState('');
+
+  const validName = value.trim().length >= 2; // disabled botão
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => { // onChange input
+    setValue(event.target.value);
+  };
+
+  const handleSearch = async () => {
+    if (validName) {
+      setShowInput(false);
+      setLoading(true);
+      try {
+        await searchAlbumsAPI(value);
+        setArtistName(value);
+      } catch (error) {
+        console.error('Error ao pesquisar', error);
+      } finally {
+        setLoading(false);
+        setShowInput(true);
+        setShowResult(true);
+        setValue('');
+      }
+    }
+  };
+
   return (
-    <p>search</p>
+    <div>
+      {showInput && (
+        <div>
+          <label htmlFor="search">Search</label>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            id="search"
+            value={ value }
+            onChange={ handleChange }
+          />
+          <button
+            data-testid="search-artist-button"
+            disabled={ !validName }
+            onClick={ handleSearch }
+          >
+            Pesquisar
+          </button>
+        </div>
+      )}
+      {loading && <Carregando />}
+      {showResult && (
+        <div>
+          <p>
+            {`Resultado de álbuns de: ${artistName}`}
+          </p>
+        </div>
+      )}
+
+    </div>
   );
 }
 
